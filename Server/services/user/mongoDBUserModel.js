0e27e0da5295b84a4954afcd222cab7e8d8da76a
Moracras <tavfikkarademir@gmail.com>
2024-09-08 02:48:30 +0300

    usermodel and password encryption added service (register onprogress)

---
+++ Server/services/user/mongoDBUserModel.js
@@ -0,0 +1,52 @@
+//@ts-check
+'use strict'
+
+const {mongoose} = require("mongoose")
+
+const passwordEncryption = require('./userutilities/passwordEncryption')
+
+
+const UserSchema = new mongoose.Schema({
+
+    username :{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true,
+
+        set: (password) => {
+            if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)) {
+                return passwordEncryption(password)
+            } else {
+                throw new Error('Password type is not correct.')
+            }
+
+    }  },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        validate: [
+            (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+            'Email type is not correct.'
+        ]
+    },
+
+    isAdmin : {
+        type:Boolean,
+        default:false,
+    }
+    
+},
+{
+    collection: 'users',
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', UserSchema)
--- Server/services/user/service.js
+++ Server/services/user/service.js
@@ -1,15 +1,54 @@
 //@ts-check
-'use strict'
+"use strict";
 
-const {ServiceBase} = require("../utilities/servicebase")
+const { ServiceBase } = require("../utilities/servicebase");
+const {User} = require("./mongoDBUserModel")
 
+class UserService extends ServiceBase {
+    static Instance = null
+  constructor() {
+    super();
+    this.Useractions();
+  }
+  async Useractions() {
+    this.router.route("/register").post(UserService.Instance.HandleRegister)
+  }
+  async HandleRegister(req, res) {
+    try {
+      const { username, email, password } = req.body;
 
-class UserService extends ServiceBase{
-    constructor(){
-        super()
-        this.Setup()
-    }
-    async Setup(){
+      if (!username || !email || !password) {
+        ServiceBase.SendBadRequestResponse(
+          res,
+          "Username, password, and email are required"
+        );
+        return;
         
+      }
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        ServiceBase.SendBadRequestResponse(res, "User already exists with this email");
+        return;
     }
+
+      
+    } catch (err) {}
+  }
+
+  async HandleLogin(req,res){
+    try {
+        
+    } catch (err) {
+        // TODO
+    }
+  }
+  async HandleLogout(req,res){
+    try {
+        
+    } catch (err) {
+        //TODO
+    }
+  }
 }
+module.exports = new UserService()
