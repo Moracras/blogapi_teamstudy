25df188ce7a4e88932fe76b719d735278284a9de
Moracras <tavfikkarademir@gmail.com>
2024-09-12 00:02:23 +0300

    token basic schema created

--- Server/services/user/userutilities/token.js
+++ Server/services/user/userutilities/token.js
@@ -1,4 +1,26 @@
 //@ts-check
 "use strict";
 
-/// 10/09/2024 todo
+const mongoose = require('mongoose')
+const TokenSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true,
+        index: true,
+    },
+
+    token: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    }
+
+}, {
+    collection: 'tokens',
+    timestamps: true
+})
